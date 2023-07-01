89487400760280572328fd9180c770e145557a8e
awen_windows <313724501@qq.com>
2023-07-01 19:58:11 +0800

    添加登录逻辑

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request
-from flaskext.mysql import MySQL
+from flask import Flask, render_template, request, redirect
+import pymysql
+import jwt
+from flask import jsonify
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
-app.config['MYSQL_DB'] = 'flask'
+app.config['SECRET_KEY'] = 'awen'
+try:
+    # 建立数据库连接
+    conn = pymysql.connect(
+    host='127.0.0.1',     # 数据库主机地址
+    user='root',          # 数据库用户名
+    password='******',    # 数据库密码
+    db='flask_test'       # 数据库名称
+)
+    print("数据库成功连接")
+except pymysql.Error as e:
+    print("数据库连接失败")
 
-mysql = MySQL()
-mysql.init_app(app)
 
 @app.route('/')
 def index():
@@ -17,18 +25,58 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        print('1')
-        print(request.json)
-        username = request.json['username']
-        password = request.json['password']
-        print(username)
-        print(password)
-        # 在此处执行登录验证逻辑
-        # 可以使用MySQL连接执行相关查询等操作
+        username = request.json.get('username')
+        password = request.json.get('password')
 
-        return 'Logged in successfully!'
+        # 连接到数据库
+        cursor = conn.cursor()
+
+        # 执行数据库查询
+        query = "SELECT * FROM user WHERE username = %s"
+        cursor.execute(query, (username,))
+
+        # 获取查询结果
+        user = cursor.fetchone()
+
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+
+        # 检查用户是否存在且密码正确
+        if user and user[1] == password:
+            # 创建 JWT
+            token = jwt.encode({'username': username}, app.config['SECRET_KEY'], algorithm='HS256')
+            # 重定向到成功页面，并将 JWT 作为参数传递
+            print('登录成功：')
+            return redirect('/success?token='+token)
+        else:
+            # 重定向到失败页面
+            return redirect('/fail')
+
     return render_template('login.html')
 
+
+# 定义需要进行权限认证的接口
+@app.route('/success')
+def success():
+    # 从请求中获取 JWT 令牌
+    token = request.args.get('token')
+
+    try:
+        # 验证 JWT 令牌
+        decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = decoded['username']
+        # 在这里可以进行相应的权限验证和业务逻辑处理
+        return 'Login successful! Welcome, ' + username
+    except jwt.DecodeError:
+        return 'Invalid token'
+    except jwt.ExpiredSignatureError:
+        return 'Token expired'
+
+
+@app.route('/fail')
+def fail():
+    return render_template('fail.html')
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
